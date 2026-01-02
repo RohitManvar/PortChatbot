256194e03229b3cd47559cd896e36ba08924b8bd
RohitManvar <Rohitmanvar123@gmail.com>
2026-01-02 20:12:21 +0530

    Updation Commit

--- Backend/rag.py
+++ Backend/rag.py
@@ -1,15 +1,23 @@
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.document_loaders import TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import TextLoader
 import os
 
 def load_rag():
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-3-small"
+    )
+
     if os.path.exists("faiss_index"):
-        return FAISS.load_local("faiss_index", OpenAIEmbeddings())
+        return FAISS.load_local(
+            "faiss_index",
+            embeddings,
+            allow_dangerous_deserialization=True
+        )
 
     loader = TextLoader("profile_docs/resume.txt")
     docs = loader.load()
 
-    db = FAISS.from_documents(docs, OpenAIEmbeddings())
+    db = FAISS.from_documents(docs, embeddings)
     db.save_local("faiss_index")
     return db
